b3e918c33962943bdb29471da3b1120cbf910a55
Swalehagameotech <zarekariswaleha@gmail.com>
2026-04-13 13:14:51 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className="text-2xl font-bold text-red-500 text-center">
+      hello world
+    </h1>
+    </>
+  )
+}
+
+export default App
